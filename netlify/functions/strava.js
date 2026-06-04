abffec49bb67c8f103161a3b32966e7df8a49ac4
SebastianRubilar <57763927+SebastianRubilar@users.noreply.github.com>
2026-06-04 10:53:17 -0400

    Update strava.js

--- netlify/functions/strava.js
+++ netlify/functions/strava.js
@@ -1,1 +1,28 @@
+exports.handler = async function(event) {
+  const stravaPath = event.path.replace('/.netlify/functions/strava', '').replace('/api/strava', '');
+  const query = event.queryStringParameters
+    ? '?' + new URLSearchParams(event.queryStringParameters).toString()
+    : '';
 
+  const url = `https://www.strava.com/api/v3${stravaPath}${query}`;
+
+  const authHeader = event.headers['authorization'] || event.headers['Authorization'];
+  if (!authHeader) {
+    return { statusCode: 401, body: JSON.stringify({ error: 'No Authorization header' }) };
+  }
+
+  try {
+    const response = await fetch(url, {
+      method: event.httpMethod || 'GET',
+      headers: { 'Authorization': authHeader, 'Content-Type': 'application/json' }
+    });
+    const data = await response.text();
+    return {
+      statusCode: response.status,
+      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+      body: data
+    };
+  } catch (err) {
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
+  }
+};
